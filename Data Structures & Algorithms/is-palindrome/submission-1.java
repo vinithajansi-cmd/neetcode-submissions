36547112bcaecba25bb60060b5ea734e216c2fbd
vinithajansi-cmd <vinithajansi@gmail.com>
2026-08-11 14:46:23 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+       int i=0;
+       int j=s.length()-1;
+
+       while(i<j){
+        while(i<j && !Character.isLetterOrDigit(s.charAt(i))){i++;}
+        while((j>i)&& !Character.isLetterOrDigit(s.charAt(j))){j--;}
+        if(Character.toLowerCase(s.charAt(i))!=Character.toLowerCase(s.charAt(j))){
+            return false;
+        }
+        i++;
+        j--;
+       }
+      return true;  
+    }
+    
+
+// public boolean alphaNum(char c){
+//     return (c>='A'&& c<='Z'||c>='a'&& c<='z'||c>='0'&&c<='9');
+// }
+}
